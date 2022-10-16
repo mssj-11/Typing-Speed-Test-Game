7d8abe70589793c46a902bb50b55dafcd0f7e482
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-10-15 23:40:43 -0500

    Typing Speed Test Game

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Typing-Speed-Test-Game
+# Typing Speed Test Game
+
+# Vista
+<p aling="center">
+    <img src="preview.png" alt="">
+</p>
+
+
+[DEMO]()
---
+++ js/paragraphs.js
@@ -0,0 +1,16 @@
+const paragraphs = [
+    // TEXT Isaac Newton
+    "Newton was the first to promulgate a set of natural laws that could govern both terrestrial (earthly) motion and celestial motion. He is associated with the scientific revolution and the advancement of heliocentrism. Newton is also credited with providing mathematical substantiation for Keplers laws of planetary motion. ",
+    // TEXT Oasis
+    "Oasis is an English rock band, formed in Manchester in 1991, led by lead guitarist and primary songwriter Noel Gallagher and his younger brother, lead vocalist and songwriter Liam Gallagher, Oasis is arguably the most successful group to emerge during the Britpop movement of the mid-1990s.",
+    // TEXT Sherlock Holmes
+    "Sherlock Holmes describes himself as a (consulting detective), which means that he is brought into cases that have proven too difficult for other investigators; we are told that he is often able to solve a problem without leaving home, although this aspect is somewhat lost in the stories themselves.",
+    // TEXT Big Bang
+    "Originally, the universe was made up of tiny, hot particles mixed with light and energy. It was nothing like what we see now. As all of that expanded and took up more space, the universe began to cool. The small particles clumped together. And they formed the atoms. Then those atoms clumped together.",
+    // Son Breaking the habit - LP
+    "Memories consume. Like opening the wound. Im picking me apart again. You all assume. Im safe here in my room. Unless I try to start again. I dont want to be the one. The battles always choose. Cause inside I realize. That Im the one confused. I dont know whats worth fighting for. Or why I have to scream.",
+    // Pumped Up Kicks -Foster the People
+    "Roberts got a quick hand. Hell look around the room, but wont tell you his plan. Hes got a rolled cigarette. Hanging out his mouth, hes a cowboy kid, yeah. He found a six-shooter gun. In his dads closet, and with a box of fun things. I dont even know what. But hes coming for you, yeah, hes coming for you.",
+    // We Will Rock You - Queen
+    "Buddy, youre a boy, make a big noise. Playing in the street, gonna be a big man someday. You got mud on your face, you big disgrace. Kicking your can all over the place, singin. We will, we will rock you. We will, we will rock you. Buddy, youre a young man, hard man. Shouting in the street, gonna take on the world someday."
+];
